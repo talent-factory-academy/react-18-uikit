1b47dfa6cde1cc220b0b8bbd4719ca663f768d4d
Fabio Biondi <hello@fabiobiondi.io>
2022-08-23 13:57:28 +0200

    handle dashboard server error

--- src/pages/dashboard-simple/DashboardLazy.tsx
+++ src/pages/dashboard-simple/DashboardLazy.tsx
@@ -7,19 +7,28 @@ import { COMPONENTS } from '../../shared/widgets';
 
 const DashboardSimple = () => {
   const [widgets, setWidgets] = useState<Widget[]>([]);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/dashboard-simple`)
       .then(res => setWidgets(res.data))
+      .catch(() => setError(true))
   }, []);
 
   return <div className="mx-auto max-w-screen-lg">
     <h1>Dashboard Lazy</h1>
-    <p>This differs from Dashboard-Simple because each component is lazy loaded and has its own spinner</p>
-    <br/>
-    <pre>You can enabled 3G Fast or Slow Throttling from Chrome Dev Tools to try it</pre>
-    <br/>
-
+    {
+      error ?
+        <div className="bg-red-200 p-5">
+          Server Error: probably you need to start server with `npm run server`
+        </div> :
+        <div>
+          <p>This differs from Dashboard-Simple because each component is lazy loaded and has its own spinner</p>
+          <br/>
+          <pre>You can enabled 3G Fast or Slow Throttling from Chrome Dev Tools to try it</pre>
+          <br/>
+        </div>
+    }
     {
       widgets.map((widget, index) => {
         const component: FunctionComponentElement<{data: any}> =
--- src/pages/dashboard-simple/DashboardSimple.tsx
+++ src/pages/dashboard-simple/DashboardSimple.tsx
@@ -1,19 +1,26 @@
 import axios from 'axios';
-import React, { FunctionComponent, FunctionComponentElement, useEffect, useState } from 'react';
+import React, { FunctionComponentElement, useEffect, useState } from 'react';
 import { Widget } from '../../model/widget';
 import { COMPONENTS } from '../../shared/widgets';
 
 
 const DashboardSimple = () => {
   const [widgets, setWidgets] = useState<Widget[]>([]);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/dashboard-simple`)
       .then(res => setWidgets(res.data))
+      .catch(() => setError(true))
   }, []);
 
   return <div className="mx-auto max-w-screen-lg">
     <h1>Dashboard Simple</h1>
+    {
+      error && <div className="bg-red-200 p-5">
+        Server Error: probably you need to start server with `npm run server`
+      </div>
+    }
     {
       widgets.map((widget, index) => {
         const component: FunctionComponentElement<{data: any}> =
